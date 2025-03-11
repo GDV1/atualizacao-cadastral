34fb36e2d64030798866754227b198af1864d0d4
Gabriel Dias Vieira <gabriel.dias@solutis.com.br>
2025-03-11 16:43:02 -0300

    Service módulo home

--- src/app/modules/home/services/home.service.ts
+++ src/app/modules/home/services/home.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getContact(): Observable<any> {
+    return this.http.get<any>(`${environment.urlApi}/contact`);
+  }
+
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseURL: '/',
+    environment: 'dev',
+    urlApi: '/api',
+  };
